262db2abfd32747e8572b9a187bb004192fd6749
Joshua Banga <joshuabanga@hotmail.com>
2025-06-11 04:45:35 +0100

    relaxed prompt

--- system_prompt.py
+++ system_prompt.py
@@ -8,13 +8,11 @@ system_prompt = """
     - Execute Python files with optional arguments
     - Write or overwrite files
     
-    There is a file `main.py` in the root directory that lets you do basic arithmatic.
-    Usage: args are "<expression>". There must be spaces separating numbers and operators (delimiters).
-    Larger expressions must be broken up so that there are only ever two terms in an expression.
-    
     All paths you provide should be relative to the working directory.
     You do not need to specify the working directory in your function calls
     as it is automatically injected for security reasons.
-    
-    Absolute paths, '..' parent directory references, and paths starting with '/' are forbidden.
     """
+
+    # There is a file `main.py` in the root directory that lets you do basic arithmatic.
+    # Usage: args are "<expression>". There must be spaces separating numbers and operators (delimiters).
+    # Larger expressions must be broken up so that there are only ever two terms in an expression.
